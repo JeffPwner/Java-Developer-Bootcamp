9112da4a47bff2090470b5f5b427f17bb1653d35
JeffPwner <eversonvssantos@gmail.com>
2023-03-31 12:15:11 -0300

    NoEncadeado & Generics

---
+++ ProjetoNo/src/NoEncadeamento/Main.java
@@ -0,0 +1,28 @@
+package NoEncadeamento;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		No<String> no1 = new No<String>("Conteúdo no1");
+		
+		No<String> no2 = new No<String>("Conteúdo no2");
+		no1.setProximoNo(no2);
+		
+		No<String> no3 = new No<String>("Conteúdo no3");
+		no2.setProximoNo(no3);
+		
+		No<String> no4 = new No<String>("Conteúdo no4");
+		no3.setProximoNo(no4);
+		
+		//no1 -> no2 -> no3 -> no4 -> null
+		
+		System.out.println(no1.getProximoNo());
+		System.out.println(no4.getProximoNo());
+		
+		System.out.println("--------------------");
+		
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo().getProximoNo());
+		
+	}
+}
